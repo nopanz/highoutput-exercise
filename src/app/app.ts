1d5a845c4c37b2be110e0d5acbadca9f5a4fcca1
Rey Anthony Aleonar <reyaleonar@gmail.com>
2019-11-12 19:34:04 +0800

    feat(*): setup mongoose ODM
    added config env

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import Koa from 'koa';
 import * as graphlSchema from './graphql';
 import { ApolloServer } from 'apollo-server-koa';
+import { PORT, DB_USERNAME, DB_PASSWORD, DB_PORT, DB_HOST, DB_NAME } from '../config/env';
+import mongoose from 'mongoose';
 
 const server = new ApolloServer(graphlSchema);
 
@@ -10,6 +12,16 @@ server.applyMiddleware({ app });
 
 export default {
   start: () => {
-    app.listen({ port: 3333 }, () => console.log(`Server ready at http://localhost:3333${server.graphqlPath}`));
+    app.listen({ port: PORT }, () => {
+      const dbUri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+      mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+      const db = mongoose.connection;
+      db.on('error', console.error.bind(console, 'connection err:'));
+      db.once('open', () => {
+        console.log('Database Connected');
+      });
+      console.log(`Server ready at http://localhost:3333${server.graphqlPath}`);
+    });
   },
 };
