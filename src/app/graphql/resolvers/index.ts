863b568030f6d337195d3ed0fc322323834f8fec
Rey Anthony Aleonar <reyaleonar@gmail.com>
2019-11-22 12:11:55 +0800

    build(graphql): include .js on merging resolvers

--- src/app/graphql/resolvers/index.ts
+++ src/app/graphql/resolvers/index.ts
@@ -2,5 +2,5 @@
 import path from 'path';
 import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
 
-const resolversArray = fileLoader(path.join(__dirname, './**/!(index.ts)'), { extensions: ['.ts'] });
+const resolversArray = fileLoader(path.join(__dirname, './**/!(index.ts)'), { extensions: ['.ts', '.js'] });
 export default mergeResolvers(resolversArray);
